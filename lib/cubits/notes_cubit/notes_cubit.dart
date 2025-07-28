284088157862404aec565415e82cd769471b51b6
Karima <elnadykarima@gmail.com>
2025-07-28 12:48:23 +0300

    add notes cubit to notes view

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesState> {
-  NotesCubitCubit() : super(NotesInitial());
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+
+      emit(NotesSuccess(noteBox.values.toList()));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
