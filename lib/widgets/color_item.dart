ce082c221eac0f7080ae1f03532e8cc7aa4e85ca
Karima <elnadykarima@gmail.com>
2025-08-26 15:00:08 +0300

    images

--- lib/widgets/color_item.dart
+++ lib/widgets/color_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isPicked, required this.itemColor});
